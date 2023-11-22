a36434212b1c0e96163ef270761e40d425b2cd0a
ABC <dinuriyaashad@gmail.com>
2023-11-22 11:02:52 +0000

    there is a new fie

---
+++ example.py
@@ -0,0 +1,9 @@
+def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()[200~def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()
